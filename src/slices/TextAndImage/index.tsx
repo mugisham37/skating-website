4e989bbaae5a1f7f657d2075999fd10c6f8a86ca
MUGISHA MOSES <144534973+mugisham37@users.noreply.github.com>
2025-06-25 13:48:46 +0300

    Added in TextAndImage slice

---
+++ src/slices/TextAndImage/index.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import {
+  PrismicRichText,
+  PrismicText,
+  SliceComponentProps,
+} from "@prismicio/react";
+import clsx from "clsx";
+import React from "react";
+import { TextAndImageSlice } from "../../../prismicio-types";
+
+import { Bounded } from "../../components/Bounded";
+import { ButtonLink } from "../../components/ButtonLink";
+import { Heading } from "../../components/Heading";
+import { SlideIn } from "../../components/SlideIn";
+import { ParallaxImage } from "./ParallaxImage";
+
+declare module "react" {
+  interface CSSProperties {
+    "--index"?: number;
+  }
+}
+
+/**
+ * Props for `TextAndImage`.
+ */
+export type TextAndImageProps = SliceComponentProps<TextAndImageSlice>;
+
+/**
+ * Component for "TextAndImage" Slices.
+ */
+const TextAndImage = ({ slice, index }: TextAndImageProps): React.ReactElement => {
+  const theme = slice.primary.theme;
+  return (
+    <Bounded
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+      className={clsx(
+        "sticky top-[calc(var(--index)*2rem)]",
+        theme === "Blue" && "bg-texture bg-brand-blue text-white",
+        theme === "Orange" && "bg-texture bg-brand-orange text-white",
+        theme === "Navy" && "bg-texture bg-brand-navy text-white",
+        theme === "Lime" && "bg-texture bg-brand-lime"
+      )}
+      style={{ "--index": index }}
+    >
+      <div className="grid grid-cols-1 items-center gap-12 md:grid-cols-2 md:gap-24">
+        <div
+          className={clsx(
+            "flex flex-col items-center gap-8 text-center md:items-start md:text-left",
+            slice.variation === "imageOnLeft" && "md:order-2"
+          )}
+        >
+          <SlideIn>
+            <Heading size="lg" as="h2">
+              <PrismicText field={slice.primary.heading} />
+            </Heading>
+          </SlideIn>
+          <SlideIn>
+            <div className="max-w-md text-lg leading-relaxed">
+              <PrismicRichText field={slice.primary.body} />
+            </div>
+          </SlideIn>
+          <SlideIn>
+            <ButtonLink
+              field={slice.primary.button}
+              color={theme === "Lime" ? "orange" : "lime"}
+            >
+              {slice.primary.button.text}
+            </ButtonLink>
+          </SlideIn>
+        </div>
+
+        <ParallaxImage
+          foregroundImage={slice.primary.foreground_image}
+          backgroundImage={slice.primary.background_image}
+        />
+      </div>
+    </Bounded>
+  );
+};
+
+export default TextAndImage;
